fc667d68d592197fe7e768dbc252c7d266901303
TakanoriMatsumoto <matscube@TakanoriMatsumoto-no-MacBook-Pro-2.local>
2014-04-27 18:40:22 +0900

    Modify kairu. 	Kairu disturb you more.

--- assets/js/kairu.js
+++ assets/js/kairu.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    $('#kairu_search').live('keypress', function(e){
-        if (e.keyCode == 13) {
-            var keyword = $('#kairu_search').val();
-            if (keyword == 'お前を消す方法') {
-                $('#kairu_box').hide();
-            } else {
-                $('#kairu_search').val('');
-            }
-        }
+    $('#kairu_box').animate({"top": "-=900px"}, 2000);
+
+    $('#kairu_search').click(function() {
+        var keyword = $('#kairu_search_keyword').val();
+//        if (keyword == 'お前を消す方法') {
+            $('#kairu_box').hide();
+//        } else {
+            $('#kairu_search_keyword').val('');
+//        }
     });
 });
